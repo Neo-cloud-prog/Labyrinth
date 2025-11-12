238be7c5313d7c91b96cf6dc59dadc6690644a1b
Neo <nnmwtn@gmail.com>
2025-11-12 13:15:46 +0100

    Add files via upload

---
+++ Labyrinth/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Labyrinth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnUP = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.lbLabyrinth = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(277, 379);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 35);
+            this.btnLeft.TabIndex = 0;
+            this.btnLeft.Text = "Left";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnUP
+            // 
+            this.btnUP.Location = new System.Drawing.Point(358, 362);
+            this.btnUP.Name = "btnUP";
+            this.btnUP.Size = new System.Drawing.Size(75, 35);
+            this.btnUP.TabIndex = 1;
+            this.btnUP.Text = "Up";
+            this.btnUP.UseVisualStyleBackColor = true;
+            this.btnUP.Click += new System.EventHandler(this.btnUP_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(358, 403);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(75, 35);
+            this.btnDown.TabIndex = 2;
+            this.btnDown.Text = "Down";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(439, 379);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(75, 35);
+            this.btnRight.TabIndex = 3;
+            this.btnRight.Text = "Right";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // lbLabyrinth
+            // 
+            this.lbLabyrinth.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbLabyrinth.Location = new System.Drawing.Point(0, 0);
+            this.lbLabyrinth.Name = "lbLabyrinth";
+            this.lbLabyrinth.Size = new System.Drawing.Size(800, 359);
+            this.lbLabyrinth.TabIndex = 4;
+            this.lbLabyrinth.Text = "label1";
+            this.lbLabyrinth.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbLabyrinth);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUP);
+            this.Controls.Add(this.btnLeft);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnUP;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Label lbLabyrinth;
+    }
+}
+
---
+++ Labyrinth/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labyrinth
+{
+    public partial class Form1 : Form
+    {
+        static string[,] LabyrinthArray =
+{
+    { "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#" },
+    { "#", "* ", "* ", "* ", "* ", "* ", "* ", "#", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "A", "#" },
+    { "#", "* ", "* ", "* ", "* ", "* ", "* ", "#", "* ", "#", "#", "#", "#", "#", "#", "#", "* ", "#" },
+    { "#", "* ", "#", "#", "#", "#", "* ", "* ", "* ", "#", "#", "#", "#", "#", "#", "#", "* ", "#" },
+    { "#", "* ", "* ", "* ", "* ", "* ", "#", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "#" },
+    { "#", "#", "#", "#", "#", "* ", "#", "* ", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#" },
+    { "#", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "#" },
+    { "#", "* ", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "* ", "#" },
+    { "#", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "#" },
+    { "#", "#", "#", "#", "#", "#", "* ", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#" },
+    { "#", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "#" },
+    { "#", "* ", "#", "#", "#", "#", "* ", "* ", "* ", "#", "#", "#", "#", "#", "#", "#", "* ", "#" },
+    {"#", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "#"},
+    {"#", "* ", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"},
+    {"#", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "#"},
+    { "#", "* ", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#" },
+    { "#", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "#" },
+    { "#", "* ", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#" },
+    { "#", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "W", " ", "#" },
+    { "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#" }
+};
+        static string LabyrinthString = "";
+
+        static int Row = LabyrinthArray.GetLength(1) - 2;
+
+        static int Col = 1;
+
+        static string NextElement = "";
+
+        void LoadLabyrinth()
+        {
+            LabyrinthString = string.Join(Environment.NewLine, Enumerable.Range(0, LabyrinthArray.GetLength(0)).Select(i => string.Join("      ", Enumerable.Range(0, LabyrinthArray.GetLength(1)).Select(j => LabyrinthArray[i, j]))));
+            lbLabyrinth.Text = LabyrinthString;
+        }
+
+        void Move(int X, int Y)
+        {
+            NextElement = LabyrinthArray[Y, X];
+            if (NextElement == "#")
+            {
+                return;
+            }
+            LabyrinthArray[Col, Row] = "* ";
+            LabyrinthArray[Y, X] = "A";
+            LoadLabyrinth();
+            Row = X;
+            Col = Y;
+            if (NextElement == "W")
+            {
+                Victory();
+            }
+        }
+
+        void Victory()
+        {
+            MessageBox.Show("Congratulations you have completed the task", "Victory", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadLabyrinth();
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            Move(Row - 1, Col);
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            Move(Row + 1, Col);
+        }
+
+        private void btnUP_Click(object sender, EventArgs e)
+        {
+            Move(Row, Col - 1);
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            Move(Row, Col + 1);
+        }
+    }
+}
